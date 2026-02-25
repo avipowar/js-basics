422f3f327ce40602b6dccfda8cf65a0d4eff4508
Avinash Powar <powaravinash06@gmail.com>
2026-02-25 10:37:38 +0530

    add notes

--- 05-create-map-filter-reduce.js
+++ 05-create-map-filter-reduce.js
@@ -1,14 +1,14 @@
 Array.prototype.myMap = function (callback) {
   let output = [];
 
-  if(typeof callback !== "function"){
+  if (typeof callback !== "function") {
     throw new TypeError("callback must be a function");
   }
 
   for (let i = 0; i < this.length; i++) {
-    if (i in this){
-        let result = callback(this[i], i, this)
-        output.push(result)
+    if (i in this) {
+      let result = callback(this[i], i, this);
+      output.push(result);
     }
   }
 
@@ -17,14 +17,12 @@ Array.prototype.myMap = function (callback) {
 
 let arr = [1, 2, 3, 4];
 
-console.log(
-  arr.myMap((curr, index, arr) => index + arr.length)
-);
-
-
+// console.log(arr.myMap((curr, index, arr) => index + arr.length));
 
 // handle with empty space
 
 let arr1 = [1, , 3];
 
-console.log(arr1.myMap(x => x * 2));
+// console.log(arr1.myMap((x) => x * 2));
+
+
